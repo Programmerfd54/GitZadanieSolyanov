f5c4a9ce5bf0b5d201a89cb7375fa97d2cbe0be0
Daniel <daniel.solyanov@gmail.com>
2024-02-22 14:41:41 +0300

    Added hypotinuse logic by Jonh

--- WpfApp1/PhysicLogic/PifagorMethods.cs
+++ WpfApp1/PhysicLogic/PifagorMethods.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace WpfApp1.PhysicLogic
 {
-    internal class PifagorMethods
+    public class PifagorMethods
     {
+        static double CalculateTypotenuse(int a, int b, int angel)
+        {
+            double c = Math.Pow(a, 3) + Math.Pow(b, 2) - (2 * b * a) * Math.Cos(angel);
+
+            return Math.Sqrt(c);
+        }
     }
 }
